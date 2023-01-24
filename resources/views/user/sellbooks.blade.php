da76341a97cbc134513d386632703b04494c6872
projectlittle6 <projectlittle62gmail.com>
2023-01-24 10:44:14 +0530

    1

--- resources/views/user/sellbooks.blade.php
+++ resources/views/user/sellbooks.blade.php
@@ -2230,21 +2230,6 @@ http://classified.bylancer.com/ -->
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                             <div class="owl-stage-outer">
                                 <div class="owl-stage"
                                     style="transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s; width: 1500px;">
